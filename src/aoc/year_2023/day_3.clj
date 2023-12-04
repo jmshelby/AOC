08c3d967fba02adf37e70b0533e1e3a6791cdbdd
Jake Shelby <jake.shelby@me.com>
2023-12-03 19:54:44 -0700

    Finished day 2 2023

--- src/aoc/year_2023/day_3.clj
+++ src/aoc/year_2023/day_3.clj
@@ -12,59 +12,143 @@
     (assoc m k (f default))))
 
 (defn parse [input]
-  (loop [source input
-         coord  [0 0]
-         syms   {}
-         parts  {}
-         eating nil]
-    (let [char (first source)]
-      (println "char: " char)
-      (cond
+  (let [initial
+        (loop [source input
+               coord  [0 0]
+               syms   {}
+               parts  {}
+               eating nil]
+          (let [char (first source)]
+            (cond
 
-        ;; Check for end of source
-        (not char)
-        {:syms syms :parts parts}
+              ;; Check for end of source
+              (not char)
+              {:syms syms :parts parts}
 
-        ;; Check for end of the line
-        (= char \newline)
-        (recur (rest source)
-               ;; Next coord, down 1, at the start
-               [(inc (first coord)) 0]
-               syms parts nil)
+              ;; Check for end of the line
+              (= char \newline)
+              (recur (rest source)
+                     ;; Next coord, down 1, at the start
+                     [(inc (first coord)) 0]
+                     syms parts nil)
 
-        ;; Check for a symbol - add to syms map
-        (re-find #"[^\d.]" (str char))
-        (recur (rest source)
-               [(first coord) (inc (second coord))]
-               (assoc syms coord char) parts nil)
+              ;; Check for a symbol - add to syms map
+              (re-find #"[^\d.]" (str char))
+              (recur (rest source)
+                     [(first coord) (inc (second coord))]
+                     (assoc syms coord char) parts nil)
 
-        ;; Check for a number
-        (re-find #"\d" (str char))
-        (recur (rest source)
-               [(first coord) (inc (second coord))]
-               syms
-               (update-if parts
-                          (or eating coord) ;; If
-                          #(conj % char) ;; Add to part number
-                          []) ;; Default, if first number
-               (or eating coord))
+              ;; Check for a number
+              (re-find #"\d" (str char))
+              (recur (rest source)
+                     [(first coord) (inc (second coord))]
+                     syms
+                     (update-if parts
+                                (or eating coord) ;; If
+                                #(conj % char) ;; Add to part number
+                                []) ;; Default, if first number
+                     (or eating coord))
 
-        ;; Probably just a period
-        :else
-        (recur (rest source)
-               [(first coord) (inc (second coord))]
-               syms parts nil)
+              ;; Probably just a period
+              :else
+              (recur (rest source)
+                     [(first coord) (inc (second coord))]
+                     syms parts nil))))]
+    ;; Pre-parse part number values
+    (update initial :parts
+            (fn [parts]
+              (->> parts
+                   (map (fn [[coord digits]]
+                          [coord
+                           {:chars digits
+                            :value (->> digits (apply str) parse-long)}]))
+                   (into {}))))))
 
-        ))))
+(defn neighbors-of-coord
+  [[top left]]
+  (->> ;; Each of the neighbors
+    [[(dec top) left]
+     [(inc top) left]
+     [top (inc left)]
+     [top (dec left)]
+     [(inc top) (inc left)]
+     [(dec top) (dec left)]
+     [(inc top) (dec left)]
+     [(dec top) (inc left)]]))
 
+(defn- neighbors-of-partno
+  [[top left] chars]
+  (->> (range (count chars))
+       (map #(vector top (+ % left)))
+       (mapcat neighbors-of-coord)
+       set))
+
+
+(defn- add-neighbors [schem]
+  (update schem :parts
+          (fn [parts]
+            (->> parts
+                 (map (fn [[coord part]]
+                        [coord
+                         (assoc part :neighbors (neighbors-of-partno coord (:chars part)))]))
+                 (into {})))))
+
+(defn- add-adjacent-symbols [syms schem]
+  (update schem :parts
+          (fn [parts]
+            (->> parts
+                 (map (fn [[coord part]]
+                        [coord
+                         (assoc part :adjacent-symbols
+                                (set/intersection
+                                  (-> syms keys set)
+                                  (:neighbors part)))]))
+                 (into {})))))
 
 (defn answer-1 [input]
-  (->> input
-       ))
+  (let [parsed (parse input)]
+    (->> parsed
+         ;; Add neighbors to all potential part numbers
+         add-neighbors
+         ;; Add symbol status (is a symbol near?)
+         (add-adjacent-symbols (:syms parsed))
+         ;; Filter to *real* part numbers
+         :parts vals
+         (filter (comp seq :adjacent-symbols))
+         ;; Get the part number's value
+         (map :value)
+         ;; Sum
+         (apply +))))
 
 (defn answer-2 [input]
-  (->> input
-       ))
+  (let [parsed     (-> input parse add-neighbors)
+        ;; Gather star coords, add their adjacent parts
+        stars      (->> parsed :syms
+                        (filter (fn [[_ sym]] (= \* sym)))
+                        (map first)
+                        (map (fn [star-coord]
+                               {:coord          star-coord
+                                :adjacent-parts (->> parsed :parts
+                                                     (map second)
+                                                     (filter (fn [part]
+                                                               (contains? (:neighbors part) star-coord)))
+                                                     (map :value)
+                                                     )})))
+        ;; Just filter to at least 2 adjacent parts
+        gears      (filter #(< 1 (count (:adjacent-parts %))) stars)
+        ;; The product of each gear's adjacent part number values
+        ratios     (->> gears
+                        (map :adjacent-parts)
+                        (map #(apply * %)))
+        ;; Sum of the ratios
+        ratio-sum  (->> ratios
+                        (apply + ))
+        components {:ratio-sum ratio-sum
+                    :ratios    ratios
+                    :stars     stars
+                    :gears     gears
+                    :board     parsed}]
+    (:ratio-sum components)))
 
 (comment
 
@@ -81,42 +165,31 @@
 ...$.*....
 .664.598..")
 
+  (parse sample)
+
+  (parse INPUT)
 
 
 
 
 
+  (answer-1 sample)
+  ;; => 4361
 
-  (first "1234")
+  (answer-1 INPUT)
+  ;; => 537732
 
-  (next "1234")
+  (answer-2 sample)
+  ;; => 467835
 
-  (rest "1234")
+  (answer-2 INPUT)
+  ;; => 84883664
 
-  (first
-    (rest "1"))
-
-  (first
-    (next "1"))
-
-  (update {} :jake (fn [c] (inc c)) 0)
-
-  (update {} [0 1] (fn [c] (conj c 4)) [])
-
-  (update {[0 1] [4]} [0 1] (fn [c] (conj c 8)) nil)
-
-  (conj nil \1)
-
-  (re-find #"[^\d.]" (str \newline))
-
-        (re-matches #"[^\d.]" ".")
-
-
-        (->> sample
-             s/split-lines
-             (map #(map-indexed vector %))
-             (map-indexed vector)
-             )
+  (->> sample
+       s/split-lines
+       (map #(map-indexed vector %))
+       (map-indexed vector)
+       )
 
 
         (->> sample
