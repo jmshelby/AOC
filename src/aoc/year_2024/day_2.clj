427e0d5141a8db98f8cf70d228b1aefa5003de58
Jake Shelby <1891693+jmshelby@users.noreply.github.com>
2024-12-02 17:46:51 -0700

    2024, Day 2: part 1 solution (pre-validation)

--- src/aoc/year_2024/day_2.clj
+++ src/aoc/year_2024/day_2.clj
@@ -5,14 +5,40 @@
 
 (def INPUT (aoc/get-my-input))
 
+(def STRAY-MARGIN 3)
+
 (defn- parse-input [in]
   (->> in
        s/split-lines
        (map #(s/split % #"\W"))
        (map #(map parse-long %))))
 
+(defn- seq-stray?
+  "Given seq of numbers, determine if element to
+  element, any abs value difference is greater than
+  the given margin."
+  [margin coll]
+  (->> coll
+       (partition 2 1)
+       (map (partial apply -))
+       (map abs)
+       (some (partial < margin))))
+
+(defn- safe-report? [report]
+  (boolean
+    (and
+      ;; Make sure we're monotonically inc/dec,
+      ;; not counting "remaining constant"
+      (or (apply > report)
+          (apply < report))
+      ;; Make sure stray inc/dec is within margin
+      (not (seq-stray? STRAY-MARGIN report)))))
+
 (defn answer-1 [input]
-  )
+  (->> input
+       parse-input
+       (filter safe-report?)
+       count))
 
 (defn answer-2 [input]
   )
@@ -28,11 +54,10 @@
 8 6 4 4 1
 1 3 6 7 9")
 
-  (parse-input sample)
-
   (parse-input INPUT)
 
   (answer-1 INPUT)
+  ;; => 369
 
   (answer-2 INPUT)
 
