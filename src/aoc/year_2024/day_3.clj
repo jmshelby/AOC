d91149390e59fd95b9e060d0e48ba878eff07a80
Jake Shelby <1891693+jmshelby@users.noreply.github.com>
2024-12-03 17:11:27 -0700

    2024, Day 3: initial empty NS

---
+++ src/aoc/year_2024/day_3.clj
@@ -0,0 +1,33 @@
+(ns aoc.year-2024.day-3
+  (:require [clojure.string :as s]
+            [clojure.set :as set]
+            [aoc.client :as aoc]))
+
+(def INPUT (aoc/get-my-input))
+
+(defn- parse-input [in]
+  )
+
+(defn answer-1 [input]
+  )
+
+(defn answer-2 [input]
+  )
+
+(comment
+
+  (->>
+    (partition 50 INPUT)
+    (map s/join)
+    )
+
+  (def sample "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
+
+  (parse-input INPUT)
+
+  (answer-1 INPUT)
+
+  (answer-2 INPUT)
+
+  ;;
+  )
