f5dd6c2d9a6a80bc2e3e03b09053d3c75b6813f9
Jake Shelby <1891693+jmshelby@users.noreply.github.com>
2024-12-05 16:53:03 -0700

    2024, Day 4: Fixed translations; made answer-1 impl fn

--- src/aoc/year_2024/day_4.clj
+++ src/aoc/year_2024/day_4.clj
@@ -37,13 +37,20 @@
           ;; No more left to check, break
           ixs)))))
 
-;; Given a rectangular grid, return new grid rotated 90 degrees
+(defn- reverse-grid [grid]
+  ;; Simple reverse of each row,
+  ;; returned as a vector
+  (mapv #(-> % reverse vec) grid))
+
+;; Given a rectangular grid, return new grid
+;; rotated 90 degrees *counter clockwise*
 (defn- rotate-grid [grid]
   (let [;; Initial cols, empty rows of cols
         init (repeat (count (first grid)) [])]
-    (reduce #(map conj %1 %2) init grid)))
+    (reduce #(mapv conj %1 (reverse %2)) init grid)))
 
-;; Given a rectangular grid, return diaganols as rows
+;; Given a rectangular grid, return diaganols as rows,
+;; rotating grid 45 degress *counter clockwise*
 (defn- diag-rows [grid]
   (let [;; Mark some important cells
         top-right    [0 (-> grid first count dec)]
@@ -52,7 +59,6 @@
         conj-in-last (fn [coll x]
                        (let [last-idx (-> coll count dec)]
                          (update coll last-idx conj x)))]
-
     (loop [;; Start w/one empty row
            rows [[]]
            ;; Start at top/right most cell
@@ -95,6 +101,30 @@
 
                    ;; Not sure why we're here
                    :else (throw (Exception. "huh?")))))))))
+
+(defn- *answer-1 [input]
+  (let [;; Parse outer example
+        grid (->> input
+                  s/split-lines
+                  (map (partial apply vector))
+                  (apply vector))
+        ;; Prep our translations
+        prep {:main         grid
+              :translations {:t1-right      grid
+                             :t1R-left      (reverse-grid grid)
+                             :t2-down       (rotate-grid grid)
+                             :t2R-up        (-> grid rotate-grid reverse-grid)
+                             :t3-down-right (-> grid diag-rows)
+                             :t3R-up-left   (-> grid diag-rows reverse-grid)
+                             :t4-down-left  (-> grid reverse-grid diag-rows)
+                             :t4R-up-right  (-> grid reverse-grid diag-rows reverse-grid)}}]
+
+    ;; Search each, and attach positions
+    (assoc prep :search (->> prep :translations
+                             (map (fn [[t rows]]
+                                    ;; For each translation, run the search
+                                    [t (map (partial vec-search KEY) rows)]))
+                             (into {})))))
 
 (defn answer-1 [input]
   )
@@ -156,6 +186,38 @@ MXMXAXMASX")
        )
   ;; => ([] [] [] [] [] [] [5] [] [] [0])
 
+  ;; Diagonal view
+  (->> sample
+       s/split-lines
+       (map (partial apply vector))
+       (apply vector) ;; gotta do this for diag-rows
+       diag-rows
+       (map (partial vec-search KEY))
+       )
+  ;; => ([] [] [] [] [] [0] [] [] [] [] [] [] [] [] [] [] [] [] [])
+
+  ;; Diagonal, reversed view
+  (->> sample
+       s/split-lines
+       (map (partial apply vector))
+       (apply vector) ;; gotta do this for diag-rows
+       diag-rows
+       (map reverse)
+       (map (partial vec-search KEY))
+       )
+  ;; => ([] [] [] [] [] [] [] [] [3] [0] [] [] [] [0] [] [0] [] [] [])
+
+  ;; Columned diagonals view
+  (->> sample
+       s/split-lines
+       (map (partial apply vector))
+       rotate-grid
+       (map sort)
+       (apply vector) ;; gotta do this for diag-rows
+       ;; diag-rows
+       ;; (map (partial vec-search KEY ))
+       )
+
 
   (->> sample
        s/split-lines
@@ -165,9 +227,45 @@ MXMXAXMASX")
        )
 
 
+
+  (let [small-sample [[1 2 3]
+                      [4 5 6]
+                      [7 8 9]]
+        ;; sample       small-sample
+        ;; Parse outer example
+        sample       (->> sample
+                          s/split-lines
+                          (map (partial apply vector))
+                          (apply vector))
+        ;; Prep our translations
+        prep         {:main         sample
+                      :translations {:t1-right      sample
+                                     :t1R-left      (reverse-grid sample)
+                                     :t2-down       (rotate-grid sample)
+                                     :t2R-up        (-> sample rotate-grid reverse-grid)
+                                     :t3-down-right (-> sample diag-rows)
+                                     :t3R-up-left   (-> sample diag-rows reverse-grid)
+                                     :t4-down-left  (-> sample reverse-grid diag-rows)
+                                     :t4R-up-right  (-> sample reverse-grid diag-rows reverse-grid)
+                                     }}]
+
+    (assoc prep :search (->> prep :translations
+                             (map (fn [[t rows]]
+                                    ;; For each translation, run the search
+                                    [t (map (partial vec-search KEY) rows)]))
+                             (into {})))
+    )
+
+
+
   (answer-1 INPUT)
 
   (answer-2 INPUT)
+
+
+
+
+
 
   ;;
   )
