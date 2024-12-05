f6d0eccccf8ca1efa4e4d142a5b7038b98cd9402
Jake Shelby <1891693+jmshelby@users.noreply.github.com>
2024-12-04 21:16:40 -0700

    2024, Day 4: WIP, oh dear i've written too much code ...

--- src/aoc/year_2024/day_4.clj
+++ src/aoc/year_2024/day_4.clj
@@ -8,6 +8,16 @@
 
 (def KEY [\X \M \A \S])
 
+;; Readability aliases for getting coords
+(def TOP first)
+(def LEFT second)
+
+(defn- inc-diag [cell]
+  ;; Just down/left movement
+  (->> cell
+       (map inc)
+       (apply vector)))
+
 (defn- vec-search [search subject]
   (let [search-c (count search)]
     (loop [ixs []
@@ -98,6 +108,67 @@ MXMXAXMASX")
        (map (partial apply vector))
        )
 
+  (let [sample       [[1 2 3]
+                      [4 5 6]
+                      [7 8 9]]
+        ;; ====
+        grid         sample
+        ;; Mark some important cells
+        top-right    [0 (-> grid first count dec)]
+        bottom-left  [(-> grid count dec) 0]
+        ;; Fns
+        conj-in-last (fn [coll x]
+                       (let [last-idx (-> coll count dec)]
+                         (update coll last-idx conj x)))
+        ]
+
+
+    (update [[1 2] [3 4] [5 6]] 2 conj 'a)
+
+    (loop [;; Start w/one empty row
+           rows [[]]
+           ;; Start at top/right most cell
+           cell top-right
+           ]
+      ;; Check if we should continue,
+      ;; stop _after_ bottom/left most cell
+      (if (= cell (inc-diag bottom-left))
+        ;; Done, Return our diaganol rows
+        rows
+
+        ;; Determine if we're still in the grid,
+        ;; (if cell doesn't exist)
+        (if (get-in grid cell)
+          ;; Add cell value to latest row,
+          ;; continue with next cell diag/down
+          (recur (conj-in-last rows (get-in grid cell))
+                 (inc-diag cell))
+          ;; Start new diag-row
+          (recur (conj rows [])
+                 ;; Determine next cell
+
+                 (cond
+                   ;; Square, start going down,
+                   ;; (middle point, where we're currently off the board bottom-right most)
+                   (apply = cell)
+                   [1 0] ;; happens to be static
+
+                   ;; Continue going down
+                   (> (TOP cell) (LEFT cell))
+                   [(inc (TOP cell)) 0]
+
+                   ;; Continue going left
+                   (> (LEFT cell) (TOP cell))
+                   [0 (dec (LEFT cell))]
+
+                   ;; Not sure why we're here
+                   :else (throw (Exception. "huh?")))
+
+                 )))))
+
+
+
+  (inc-diag [0 0])
 
   (->> INPUT
        s/split-lines
@@ -116,3 +187,19 @@ MXMXAXMASX")
 
   ;;
   )
+
+
+
+((\M \S \A \M \x \X \S \M \M \M)
+ (\A \S \M \S \M \X \M \A \S \M \J  )
+ (\M \M \A \A \M \X \S \X \M \A)
+ (\X \M \S \M \S \A \M \A \S \M)
+ (\M \M \A \X \M \A \S \A \M \X)
+ (\A \M \A \X \X \M \M \A \X \X    )
+ (\S \S \X \S \A \S \M \S \M \S \J    )
+ (\A \A \A \S \A \M \A \X \A \S)
+ (\m \M \M \M \X \M \M \M \A \M)
+ (\X \S \A \M \X \A \X \M \X \M  )
+ (         \J)
+
+ )
